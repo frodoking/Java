42d75c17f1fbf4fd80f5c8931dae0d3023628031
frodo <awangyun8@gmail.com>
2019-08-02 17:45:19 +0800

    Test GC for (3YGC ==> 2FGC ==> 2YGC)
    [GC (Allocation Failure) [PSYoungGen: 174346K->34107K(218624K)] 174346K->165187K(722432K), 0.0874647 secs] [Times: user=0.09 sys=0.13, real=0.09 secs] 
    [GC (Allocation Failure) [PSYoungGen: 201343K->33838K(218624K)] 332423K->328758K(722432K), 0.1024653 secs] [Times: user=0.11 sys=0.11, real=0.11 secs] 
    [GC (Allocation Failure) [PSYoungGen: 176858K->34184K(218624K)] 471778K->460184K(722432K), 0.0653997 secs] [Times: user=0.09 sys=0.13, real=0.08 secs] 
    [Full GC (Ergonomics) [PSYoungGen: 34184K->0K(218624K)] [ParOldGen: 426000K->460099K(503808K)] 460184K->460099K(722432K), [Metaspace: 3803K->3803K(1056768K)], 0.1227318 secs] [Times: user=0.23 sys=0.00, real=0.11 secs] 
    [Full GC (Ergonomics) [PSYoungGen: 168894K->0K(218624K)] [ParOldGen: 460099K->1318K(503808K)] 628993K->1318K(722432K), [Metaspace: 3804K->3804K(1056768K)], 0.0130313 secs] [Times: user=0.00 sys=0.02, real=0.02 secs] 
    [GC (Allocation Failure) [PSYoungGen: 166936K->32768K(218624K)] 168255K->165158K(722432K), 0.0235502 secs] [Times: user=0.05 sys=0.00, real=0.02 secs] 
    [GC (Allocation Failure) [PSYoungGen: 199823K->32768K(218624K)] 332214K->328998K(722432K), 0.0339613 secs] [Times: user=0.06 sys=0.00, real=0.03 secs]

---
+++ java_knowledge/src/main/java/cn/com/frodo/knowledge/GCTest.java
@@ -0,0 +1,45 @@
+package com.frodo.gc;
+
+import com.frodo.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Description 3YGC ==> 2FGC ==> 2YGC
+ * Start Args -Xms748m -Xmx748m -Xmn256m -XX:NewRatio=4 -XX:SurvivorRatio=4 -XX:MaxTenuringThreshold=3 -XX:+PrintGCDetails
+ * eden 128, s0/s1 32, old = (eden+s0)*3 = 480, tenure=64 (fixed)
+ * @author frodoking
+ */
+public class GCTest implements Test
+{
+    @Override public void doTest()
+    {
+        int _1MB = 1024 * 1024;
+        List caches = new ArrayList();
+        for (int i = 0; i < 15; i++)
+        {
+            caches.add(new byte[32 * _1MB]);
+            // System.out.println("ygc.---" + i);
+        }
+
+        for (int i = 0; i < 4; i++)
+        {
+            caches.add(new byte[32 * _1MB]);
+            // System.out.println("fgc 2.---" + i);
+        }
+
+        for (int i = 0; i < 19; i++)
+        {
+            caches.remove(0);
+        }
+
+        for (int i = 0; i < 10; i++)
+        {
+            caches.add(new byte[32 * _1MB]);
+            // System.out.println("ygc 2.---" + i);
+        }
+        caches.add(new byte[8 * _1MB]);
+
+    }
+}
