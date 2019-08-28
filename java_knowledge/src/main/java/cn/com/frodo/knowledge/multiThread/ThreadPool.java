182404c8fe3d36dbcc9567969c1d8f34031ce960
frodo <awangyun8@gmail.com>
2019-08-28 09:42:09 +0800

    Update ThreadPool.java

--- java_knowledge/src/main/java/cn/com/frodo/knowledge/multiThread/ThreadPool.java
+++ java_knowledge/src/main/java/cn/com/frodo/knowledge/multiThread/ThreadPool.java
@@ -1,16 +1,43 @@
 package cn.com.frodo.knowledge.multiThread;
 
+import com.frodo.Test;
+
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
-public class ThreadPool{
-	ExecutorService executorService;
+/**
+ * Description 触发条件：线程数=maximumPoolSize 且 queue已满
+ * @author frodoking
+ * @version [V1, 2019/8/2 14:13]
+ */
+public class ThreadPoolTest implements Test
+{
+    @Override public void doTest()
+    {
+        ExecutorService executorService =  new ThreadPoolExecutor(4, 14,10L, TimeUnit.MICROSECONDS, new LinkedBlockingQueue(10));
 
-	public ThreadPool() {
-		executorService = Executors.newFixedThreadPool(3);
-	}
-	
-	
-	
+        for (int i = 0; i < 25; i++)
+        {
+            final int index = i;
+            executorService.submit(new Runnable()
+            {
+                @Override public void run()
+                {
+                    try
+                    {
+                        Thread.sleep(10000);
+                    }
+                    catch (InterruptedException e)
+                    {
+                        e.printStackTrace();
+                    }
+                    System.out.println(".....execute " + index + " " + System.currentTimeMillis());
+                }
+            });
+        }
 
+    }
 }
+
