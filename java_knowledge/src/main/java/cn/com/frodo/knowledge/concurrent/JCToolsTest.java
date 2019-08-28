1c7f1a49108134ff89bb52a4827aa93c47e886f6
frodo <awangyun8@gmail.com>
2019-08-28 09:49:52 +0800

    Create JCToolsTest.java

---
+++ java_knowledge/src/main/java/cn/com/frodo/knowledge/concurrent/JCToolsTest.java
@@ -0,0 +1,36 @@
+package com.frodo.concurrent;
+
+import org.jctools.queues.MpscUnboundedArrayQueue;
+
+/**
+ * Description TODO
+ * @author frodoking
+ * @version [V1, 2019/8/12 21:03]
+ */
+public class JCToolsTest extends ConcurrentQueue
+{
+
+    MpscUnboundedArrayQueue<ConcurrentQueue.Call> queue = new MpscUnboundedArrayQueue<>(1024*16);
+
+    public JCToolsTest()
+    {
+        super();
+    }
+
+    public JCToolsTest(int producerNum, int producerWaitTime, int consumerNum, long callTime)
+    {
+        super(producerNum, producerWaitTime, consumerNum, callTime);
+    }
+
+    @Override Call poll()
+    {
+        return queue.poll();
+    }
+
+    @Override void offer(Call call)
+    {
+        queue.offer(call);
+        //System.out.println(queue.size());
+    }
+
+}
