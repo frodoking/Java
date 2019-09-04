10a32a5d42c582ce0e0be2158426097654089767
frodo <awangyun8@gmail.com>
2019-09-04 10:24:06 +0800

    Create SPILoard.java

---
+++ java_knowledge/src/main/java/cn/com/frodo/knowledge/spi/SPILoard.java
@@ -0,0 +1,19 @@
+package com.frodo.spi;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SPILoard implements SPIInterface
+{
+    private static final Logger LOGGER = LoggerFactory.getLogger(SPILoard.class);
+
+    @Override public void executeMethodOne()
+    {
+        LOGGER.info(">> {}, hashcode: {}", "executeMethodOne", hashCode());
+    }
+
+    @Override public void executeMethodTwo()
+    {
+        LOGGER.info(">> {}, hashcode: {}", "executeMethodTwo", hashCode());
+    }
+}
