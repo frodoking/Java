ffb5ae3a9de642c296c3d49803b1174756acdfa6
frodo <awangyun8@gmail.com>
2019-09-04 10:24:24 +0800

    Create SPIQueen.java

---
+++ java_knowledge/src/main/java/cn/com/frodo/knowledge/spi/SPIQueen.java
@@ -0,0 +1,20 @@
+package com.frodo.spi;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SPIQueen implements SPIInterface
+{
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SPIQueen.class);
+
+    @Override public void executeMethodOne()
+    {
+        LOGGER.info(">> {}, hashcode: {}", "executeMethodOne", hashCode());
+    }
+
+    @Override public void executeMethodTwo()
+    {
+        LOGGER.info(">> {}, hashcode: {}", "executeMethodTwo", hashCode());
+    }
+}
