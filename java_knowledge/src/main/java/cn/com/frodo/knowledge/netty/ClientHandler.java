02c06866bae01749839f9a2f537f7baaa0901c01
frodo <awangyun8@gmail.com>
2019-09-26 17:50:13 +0800

    Create ClientHandler.java

---
+++ java_knowledge/src/main/java/cn/com/frodo/knowledge/netty/ClientHandler.java
@@ -0,0 +1,44 @@
+package com.frodo.netty;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
+import org.apache.commons.io.Charsets;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ClientHandler extends ChannelInboundHandlerAdapter
+{
+    private static final Logger LOGGER = LoggerFactory.getLogger(ClientHandler.class);
+
+    @Override public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception
+    {
+        try
+        {
+            ByteBuf bb = (ByteBuf) msg;
+            byte[] respByte = new byte[bb.readableBytes()];
+            bb.readBytes(respByte);
+            String respStr = new String(respByte, Charsets.UTF_8);
+            //LOGGER.warn("client--收到响应 [ {} ]" , respStr);
+
+        }
+        finally
+        {
+            ReferenceCountUtil.release(msg);
+        }
+    }
+
+    @Override public void channelReadComplete(ChannelHandlerContext ctx) throws Exception
+    {
+        //LOGGER.info("客户端读取数据完毕");
+    }
+
+    @Override public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception
+    {
+        //LOGGER.error("client 读取数据出现异常");
+        ctx.close();
+    }
+
+}
+
