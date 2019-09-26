58a604606c2f487a3ac273165efe444a8842b129
frodo <awangyun8@gmail.com>
2019-09-26 17:41:13 +0800

    Create ProtocolDecoder.java

---
+++ java_knowledge/src/main/java/cn/com/frodo/knowledge/encodedecode/pb/ProtocolDecoder.java
@@ -0,0 +1,23 @@
+package com.frodo.encodedecode.pb;
+
+import io.protostuff.ProtostuffIOUtil;
+import io.protostuff.Schema;
+import io.protostuff.runtime.RuntimeSchema;
+
+public class ProtocolDecoder
+{
+    <T> T decode(byte[] bytes, Class<T> clazz) {
+        T obj = null;
+        try {
+            obj = clazz.newInstance();
+            Schema schema = RuntimeSchema.getSchema(obj.getClass());
+            ProtostuffIOUtil.mergeFrom(bytes, obj, schema);
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+        return obj;
+    }
+}
