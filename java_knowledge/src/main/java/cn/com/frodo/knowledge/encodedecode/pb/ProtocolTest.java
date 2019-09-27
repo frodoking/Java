f50d83c539ae33f670ad9959d163638d649da458
frodo <awangyun8@gmail.com>
2019-09-27 16:37:19 +0800

    Update ProtocolTest.java

--- java_knowledge/src/main/java/cn/com/frodo/knowledge/encodedecode/pb/ProtocolTest.java
+++ java_knowledge/src/main/java/cn/com/frodo/knowledge/encodedecode/pb/ProtocolTest.java
@@ -1,6 +1,7 @@
 package com.frodo.encodedecode.pb;
 
 import com.frodo.Test;
+import io.protostuff.Tag;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,6 +19,7 @@ public class ProtocolTest implements Test
         pojo.key2 = "hahaxxxx";
         pojo.value = "hoho";
         pojo.value2 = "hohoxxxxx";
+        pojo.value3 = "hohosssss";
         byte[] bytes = encoder.encode(pojo);
         POJOE pojose = decoder.decode(bytes, POJOE.class);
         LOGGER.info("key {}, value {}", pojose.key + ", " + pojose.key2, pojose.value + ", " + pojose.value2);
@@ -25,26 +27,35 @@ public class ProtocolTest implements Test
 
     public static class POJO
     {
+        @Tag(100)
         String key;
+        @Tag(200)
         String value;
     }
 
     public static class POJOE extends POJO
     {
+        @Tag(101)
         String key2;
+        @Tag(203)
         String value2;
     }
 
     public static class POJOS extends POJO
     {
+        @Tag(100)
         String key;
+        @Tag(200)
         String value;
+        @Tag(203)
         String value3;
     }
 
     public static class POJOSE extends POJOS
     {
+        @Tag(value = 101, alias = "aa")
         String key2;
+        @Tag(202)
         String value2;
     }
 }
