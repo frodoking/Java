39df5116918da154a721bb071592af544715d5f2
frodo <awangyun8@gmail.com>
2019-09-30 14:34:42 +0800

    Update Main.java

--- java_knowledge/src/main/java/cn/com/frodo/knowledge/Main.java
+++ java_knowledge/src/main/java/cn/com/frodo/knowledge/Main.java
@@ -19,96 +19,47 @@ public class Main
 
     public static void main(String[] args)
     {
-        MockInterface test;
-        if (args.length == 0) {
-            test = new BasicObjectTest();
-            test.doTest();
-            return;
-        }
-        String cmd = args[0];
-        switch (cmd)
+        Object flag = Configuration.get("main.default");
+        Object object = Configuration.get("main.list." + flag.toString());
+        try
         {
-            case "gc":
-                test = new GCTest();
-                break;
-            case "clq":
-                if (args.length == 5)
+            MockInterface test = null;
+            String testClassName = ((LinkedHashMap) object).get("name").toString();
+            System.out.println(testClassName);
+            ((LinkedHashMap) object).remove("name");
+            Class clazz = Class.forName(testClassName);
+            if (((LinkedHashMap) object).isEmpty())
+            {
+                test = (Test) clazz.newInstance();
+            }
+            else
+            {
+                Constructor[] constructors = clazz.getConstructors();
+                for (Constructor constructor : constructors)
                 {
-                    test = new ConcurrentLinkedQueueTest(Integer.parseInt(args[1]), Integer.parseInt(args[2]),
-                            Integer.parseInt(args[3]), Long.parseLong(args[4]));
+                    if (constructor.getParameters().length == ((LinkedHashMap) object).size())
+                    {
+                        Parameter[] p = constructor.getParameters();
+                        Object[] argss = new Object[p.length];
+                        int index = 0;
+                        for (Object argKey : ((LinkedHashMap) object).keySet())
+                        {
+                            Object argValue = ((LinkedHashMap) object).get(argKey);
+                            argss[index] = argValue;
+                            index++;
+                        }
+                        test = (Test) constructor.newInstance(argss);
+                    }
                 }
-                else
-                {
-                    test = new ConcurrentLinkedQueueTest();
-                    System.out.println("int producerNum, int producerWaitTime, int consumerNum, long callTime");
-                }
-                break;
-            case "lbq":
-                if (args.length == 5)
-                {
-                    test = new LinkedBlockingQueueTest(Integer.parseInt(args[1]), Integer.parseInt(args[2]),
-                            Integer.parseInt(args[3]), Long.parseLong(args[4]));
-                }
-                else
-                {
-                    test = new LinkedBlockingQueueTest();
-                    System.out.println("int producerNum, int producerWaitTime, int consumerNum, long callTime");
-                }
-                break;
-            case "dis":
-                if (args.length == 5)
-                {
-                    test = new DisruptorTest(Integer.parseInt(args[1]), Integer.parseInt(args[2]),
-                            Integer.parseInt(args[3]), Long.parseLong(args[4]));
-                }
-                else
-                {
-                    test = new DisruptorTest();
-                    System.out.println("int producerNum, int producerWaitTime, int consumerNum");
-                }
-                break;
-            case "jct":
-                if (args.length == 5)
-                {
-                    test = new JCToolsTest(Integer.parseInt(args[1]), Integer.parseInt(args[2]),
-                            Integer.parseInt(args[3]), Long.parseLong(args[4]));
-                }
-                else
-                {
-                    test = new JCToolsTest();
-                    System.out.println("int producerNum, int producerWaitTime, int consumerNum");
-                }
-                break;
-            case "gson":
-                if (args.length == 2)
-                {
-                    test = new GsonTest(Integer.parseInt(args[1]));
-                }
-                else
-                {
-                    test = new GsonTest();
-                }
-                break;
-            case "gsont":
-                if (args.length == 2)
-                {
-                    test = new GsonMultiThreadTest(Integer.parseInt(args[1]));
-                }
-                else
-                {
-                    test = new GsonMultiThreadTest();
-                }
-                break;
-            case "tp":
-                test = new ThreadPoolTest();
-                break;
-            case "ty":
-                test = new ThreadYieldTest();
-                break;
-            default:
-                test = new BasicObjectTest();
-                break;
+            }
+            if (test != null)
+            {
+                test.doTest();
+            }
         }
-        test.doTest();
+        catch (ClassNotFoundException | IllegalAccessException | InstantiationException | InvocationTargetException e)
+        {
+            e.printStackTrace();
+        }
     }
 }
