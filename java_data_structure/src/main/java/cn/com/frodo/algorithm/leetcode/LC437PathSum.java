95856937e9ca1ecbcadac5e5736200a4da290a8c
frodoking <A07127014s>
2023-03-21 20:54:59 +0800

    update leetcode 0321.

---
+++ java_data_structure/src/main/java/cn/com/frodo/algorithm/leetcode/LC437PathSum.java
@@ -0,0 +1,67 @@
+package cn.com.frodo.algorithm.leetcode;
+
+
+import cn.com.frodo.BinaryTreeNode;
+import cn.com.frodo.algorithm.IAlgorithm;
+import org.junit.Assert;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * 437. 路径总和 III
+ * 给定一个二叉树的根节点 root ，和一个整数 targetSum ，求该二叉树里节点值之和等于 targetSum 的 路径 的数目。
+ *
+ * 路径 不需要从根节点开始，也不需要在叶子节点结束，但是路径方向必须是向下的（只能从父节点到子节点）。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ *
+ * 输入：root = [10,5,-3,3,2,null,11,3,-2,null,1], targetSum = 8
+ * 输出：3
+ * 解释：和等于 8 的路径有 3 条，如图所示。
+ * 示例 2：
+ *
+ * 输入：root = [5,4,8,11,null,13,4,7,2,null,null,5,1], targetSum = 22
+ * 输出：3
+ */
+@LCPoint(difficulty = LCPoint.Difficulty.medium, company = LCPoint.Company.bytedance,
+        category = LCPoint.Category.tree)
+public class LC437PathSum implements IAlgorithm {
+    @Override
+    public void exec() {
+//        int[] array = {5, 4, 8, 11, BinaryTreeNode._null.data, 13, 4, 7, 2, BinaryTreeNode._null.data, BinaryTreeNode._null.data, 5, 1};
+//        int[] array = {-2, BinaryTreeNode._null.data, -3};
+        int[] array = {1, BinaryTreeNode._null.data,
+                2, BinaryTreeNode._null.data,
+                3, BinaryTreeNode._null.data};
+        BinaryTreeNode root = BinaryTreeNode.build(array);
+
+        Assert.assertEquals(2, pathSum(root, 3));
+    }
+
+    private AtomicInteger counter = new AtomicInteger();
+    public int pathSum(BinaryTreeNode root, int targetSum) {
+        if (root == null) return 0;
+        dfs(root, targetSum, counter);
+        pathSum(root.left, targetSum);
+        pathSum(root.right, targetSum);
+
+        return counter.get();
+    }
+
+    public void dfs(BinaryTreeNode root, int targetSum, AtomicInteger counter) {
+        if (root == null) {
+            return;
+        }
+
+        if (root.data == targetSum) {
+            counter.incrementAndGet();
+        }
+
+        dfs(root.left, targetSum - root.data, counter);
+        dfs(root.right, targetSum - root.data, counter);
+    }
+}
---
+++ java_data_structure/src/main/java/cn/com/frodo/algorithm/leetcode/LC494FindTargetSumWays.java
@@ -0,0 +1,70 @@
+package cn.com.frodo.algorithm.leetcode;
+
+import cn.com.frodo.algorithm.IAlgorithm;
+import org.junit.Assert;
+
+import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * 494. 目标和
+ * 给你一个整数数组 nums 和一个整数 target 。
+ * <p>
+ * 向数组中的每个整数前添加 '+' 或 '-' ，然后串联起所有整数，可以构造一个 表达式 ：
+ * <p>
+ * 例如，nums = [2, 1] ，可以在 2 之前添加 '+' ，在 1 之前添加 '-' ，然后串联起来得到表达式 "+2-1" 。
+ * 返回可以通过上述方法构造的、运算结果等于 target 的不同 表达式 的数目。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [1,1,1,1,1], target = 3
+ * 输出：5
+ * 解释：一共有 5 种方法让最终目标和为 3 。
+ * -1 + 1 + 1 + 1 + 1 = 3
+ * +1 - 1 + 1 + 1 + 1 = 3
+ * +1 + 1 - 1 + 1 + 1 = 3
+ * +1 + 1 + 1 - 1 + 1 = 3
+ * +1 + 1 + 1 + 1 - 1 = 3
+ * 示例 2：
+ * <p>
+ * 输入：nums = [1], target = 1
+ * 输出：1
+ */
+@LCPoint(difficulty = LCPoint.Difficulty.medium,
+        category = LCPoint.Category.tree)
+public class LC494FindTargetSumWays implements IAlgorithm {
+    @Override
+    public void exec() {
+        int[] nums = {1,0};
+        int target = 1;
+
+        Assert.assertEquals(2, findTargetSumWays(nums, target));
+    }
+
+    private AtomicInteger counter = new AtomicInteger();
+
+    public int findTargetSumWays(int[] nums, int target) {
+        if (nums.length == 1 && (nums[0] + target == 0 || nums[0] - target == 0)) {
+            return 1;
+        }
+        dfs(nums, 0, target);
+
+        return counter.get();
+    }
+
+    public void dfs(int[] nums, int index, int target) {
+        if (index >= nums.length) {
+            if (target == 0) {
+                counter.incrementAndGet();
+            }
+        } else {
+            System.out.println("index = " + index + ", target = " + target);
+
+            int num = nums[index];
+            dfs(nums, index + 1, target + num);
+            dfs(nums, index + 1, target - num);
+        }
+    }
+}
